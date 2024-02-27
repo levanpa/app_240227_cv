1a63e4584c327a91e36fb45e7c70711285803d2d
Kai Pa <levanpa00@gmail.com>
2024-02-27 22:01:12 +0700

    setup file structure

---
+++ src/components/MainPage.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+// defineProps({
+//   msg: String,
+// })
+
+// const count = ref(0)
+</script>
+
+<template lang="pug">
+</template>
+
+<style lang="sass">
+@import '../sass/mainpage.sass'
+</style>
+
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue'
+</script>
+
+<template lang="pug">
+</template>
+
+<style lang="sass">
+@import '../sass/mainpage.sass'
+</style>
