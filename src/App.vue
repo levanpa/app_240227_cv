49bb6025f2b0ffaaa56bb7f50897a306a642f753
Kai Pa <levanpa00@gmail.com>
2024-03-01 12:34:59 +0700

    open sidebar on mobile

--- src/App.vue
+++ src/App.vue
@@ -3,136 +3,61 @@ import { onMounted } from 'vue'
 import Sidebar from './components/Sidebar.vue'
 import MainPage from './components/MainPage.vue'
 
+let topPosition = 0
+
 function loaded() {
   setTimeout(() => {
     document.querySelector('.openning-gradient').classList.add('animating')
   }, 100);
 }
 
+function handleCompactHeader() {
+  // execute event when scoll 100px from top
+  document.addEventListener('scroll', (event) => {
+    if (window.scrollY > 100 && !document.querySelector('.page-wrapper.show-sidebar')) {
+      document.querySelector('.top-wrapper').classList.add('is-compact')
+    } else {
+      document.querySelector('.top-wrapper').classList.remove('is-compact')
+    }
+  })
+}
+
+function toggleSidebar() {
+  const element = document.querySelector('.page-wrapper')
+
+  if (element.classList.contains('show-sidebar')) {
+    // scroll to position before open sidebar
+    window.scrollTo(0, topPosition)
+    element.classList.remove('show-sidebar')
+  } else {
+    // get current position to scroll to after close sidebar
+    topPosition = window.scrollY
+    element.classList.add('show-sidebar')
+    window.scrollTo(0, 0)
+  }
+}
+
 onMounted(() => {
   loaded()
+  handleCompactHeader()
 })
 </script>
 
 <template lang="pug">
 .page-wrapper
-  .top-wrapper.mb
+  .top-wrapper.mb(@click.prevent="toggleSidebar")
     .avatar
       img(src="./assets/me.png" alt="Le Van Pa" title="I'm older than this image")
     .right
       h1.name LE VAN PA
       span.born Born in 1995
+    .contact-button
+      i.fa-regular.fa-address-card
   Sidebar
   MainPage
   .openning-gradient
 </template>
 
 <style lang="sass">
-@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap')
-@import url('./sass/normalize.css')
-
-// reset css
-*
-  margin: 0
-  padding: 0
-  box-sizing: border-box
-  line-height: 1.2
-
-button
-  background: none
-  border: none
-
-h1, h2, h3, h4, h5, h6
-  font-size: inherit
-  font-weight: normal
-  margin: 0
-
-li
-  list-style-type: none
-
-img
-  border: none
-  width: 100%
-  height: 100%
-
-a
-  color: inherit
-  text-decoration: none
-
-// general styles
-#app
-  width: 100%
-  max-width: 1024px
-  margin: 0 auto
-  font-family: 'Open Sans', sans-serif
-  font-size: 16px
-  opacity: 1
-  .hidden
-    visibility: hidden
-.page-wrapper
-  display: flex
-  box-shadow: 2px 0 7px #a9a9a9
-  position: relative
-  overflow: hidden
-  .sidebar-wrapper
-    flex: 35% 1 1
-    background-color: #ecf2f4
-  .mainpage-wrapper
-    flex: 65% 1 1
-
-.openning-gradient
-  width: 100%
-  height: 150%
-  background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 50%, rgba(255, 255, 255, 1) 100%)
-  position: absolute
-  top: -50%
-  left: 0
-  z-index: 100
-  transition: top 1s ease-out
-  &.animating
-    top: 100%
-
-+pc
-  .mb
-    display: none !important
-
-+mb
-  html
-    font-size: calc(100vw / 375)
-  body
-    overflow-x: hidden
-    font-size: 16rem
-  .pc
-    display: none !important
-
-  #app
-    max-width: unset
-    font-size: 16rem
-  .page-wrapper
-    flex-direction: column
-    box-shadow: 2rem 0 7rem #a9a9a9
-    .sidebar-wrapper
-      display: none
-    .mainpage-wrapper
-      flex: 100% 1 1
-
-    > .top-wrapper
-        display: flex
-        align-items: center
-        gap: 10rem
-        padding: 20rem 40rem
-        background-color: #ecf2f4
-        border-bottom: 1rem solid #bbb
-        .avatar
-          flex-basis: 100rem
-          height: 100rem
-          border-radius: 50%
-          background-color: #fff
-        .name
-          font-weight: 600
-          font-size: 20rem
-        .born
-          color: #888
-          font-size: 14rem
-
+@import url('./sass/app.sass')
 </style>
