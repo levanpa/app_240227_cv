5b7dcea15db29ee5c700157cf3c525156a8305ef
Kai Pa <levanpa00@gmail.com>
2024-03-12 11:33:23 +0700

    update modal for soft skills

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,5 +1,7 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+
+let modal = ref(null)
 
 function copyToClipboard(event) {
   if (!navigator.clipboard) {
@@ -29,6 +31,41 @@ function showNotification(selector, relativeElement) {
 function bait(event) {
   showNotification('.baited-notity', event.target)
 }
+
+function handleModal() {
+  modal = document.querySelector('.soft-skills-modal')
+  modal.addEventListener('click', event => {
+    const bounding = modal.getBoundingClientRect()
+    if (
+      event.clientX < bounding.left ||
+      event.clientX > bounding.right ||
+      event.clientY < bounding.top ||
+      event.clientY > bounding.bottom
+    ) {
+      closeModal()
+    }
+  })
+  modal.addEventListener('close', event => {
+    document.querySelector('body').classList.remove('is-modal-open')
+  })
+}
+
+function expandSoftSkills() {
+  modal.showModal()
+  document.querySelector('body').classList.add('is-modal-open')
+}
+
+function closeModal() {
+  modal.classList.add('closing')
+  modal.addEventListener('animationend', () => {
+    modal.classList.remove('closing')
+    modal.close()
+  }, { once: true })
+}
+
+onMounted(() => {
+  handleModal()
+})
 </script>
 
 <template lang="pug">
@@ -39,6 +76,7 @@ function bait(event) {
     .right
       h1.name LE VAN PA
       span.born Born in 1995
+
   ul.info-wrapper
     li.info-item
       i.icon.fa-solid.fa-square-phone
@@ -58,6 +96,7 @@ function bait(event) {
         a.text(target="_blank" href="https://maps.app.goo.gl/7Mm6bywZanWTK2jj6") Thanh Da, Binh Thanh
         button.copy-button(@click.prevent="copyToClipboard" title="copy to clipboard")
           i.fa-regular.fa-copy
+
   ul.contact-wrapper
     li.contact-item
       i.icon.fa-brands.fa-facebook-messenger
@@ -83,20 +122,40 @@ function bait(event) {
           |t.me/levanpa00
         button.copy-button(@click.prevent="copyToClipboard" title="copy to clipboard")
           i.fa-regular.fa-copy
+
   .soft-skills-wrapper
     .title-wrapper
       i.caret-icon.fa-solid.fa-caret-down(@click.prevent="bait")
       i.icon.fa-solid.fa-universal-access
       h2.title soft skills
-    ul.list
+      button.common-button(aria-label="expand soft skills button" @click.prevent="expandSoftSkills")
+        i.fa-solid.fa-up-down
+    .list
       li.item Experience in team working.
       li.item Research, learn new things.
       li.item Have responsibility for work.
       li.item Self-management.
       li.item Support and share.
-  .add-skills-wrapper
-    ul.list
-      li.item
+    dialog.soft-skills-modal
+      button.close-button(aria-label="close modal button" @click="closeModal")
+        i.fa-regular.fa-circle-xmark
+      ul.modal-list
+        li.modal-item
+          p.title Experience in team working.
+          p.description làm việc theo nhóm là kỹ năng quan trọng để có thể kết hợp từng người lại để hoàn thành được dự án một cách có hiệu quả nhất. Tôi đã rèn luyện kỹ năng này trong suốt quá trình làm việc của mình.
+        li.modal-item
+          p.title Research, learn new things.
+          p.description luôn luôn cập nhật công nghệ mới để nâng cao hiệu suất cho dự án của mìnhm kỹ năng sống còn của lập trình viên
+        li.modal-item
+          p.title Have responsibility for work.
+          p.description luôn luôn cập nhật công nghệ mới để nâng cao hiệu suất cho dự án của mìnhm kỹ năng sống còn của lập trình viên
+        li.modal-item
+          p.title Self-management.
+          p.description kỹ năng làm việc độc lập, tự quản lý công việc của mình qua Trello, Google Sheet
+        li.modal-item
+          p.title Support and share.
+          p.description Luôn sẵn sàng chia sẻ kiến thức mới lạ lên knowledge book của công ty, cũng như chia sẻ, hướng dẫn trực tiếp cho nhân viên mới
+
   .qr-wrapper
     a.qr-image(href="https://github.com/levanpa/app_240227_cv/tree/build" target="_blank")
       img(src="../assets/qr_to_github.png" alt="levanpa cv github")
@@ -108,6 +167,7 @@ function bait(event) {
     span.text Copied
   .baited-notity
     span.text You've got click baited!
+
 </template>
 
 <style lang="sass">
